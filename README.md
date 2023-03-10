ce69d64e2a26949835e11e4898309472a6f0f62f
miccai1312 <126781942+miccai1312@users.noreply.github.com>
2023-03-10 11:16:15 +0100

    update

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Code for submission miccai 2023 #1312
 
-Here the code to reproduce the work: "DAS-MIL: Distilling Across Scales for Multiple Instance Learning Classification of Histological WSIs"
+Here the code to reproduce the work: "Distilling Across Scales for MIL Classification of Histological WSIs"
 
 Steps:
 1. `conda env create -f environment.yml`
 2. `python main.py`
+
+the experiments are inside utils.experiments.py
--- utils/process.py
+++ utils/process.py
@@ -1,6 +1,6 @@
 from utils.seed import init_seed
 from utils.datasets import get_loaders
-from utils.test import test
+from utils.training import train
 from utils.models import selectModelMulti
 
 def processDataset(args):
@@ -10,4 +10,4 @@ def processDataset(args):
     train_loader,val_loader,test_loader=get_loaders(args)
     model= selectModelMulti(model_name=args.modeltype,num_layers=args.num_layers,c_hidden=args.c_hidden,temperature=args.temperature,beta=args.beta,lamb=args.lamb,layer_name=args.layer_name,c_out=args.n_classes,input_size=args.input_size,residual=args.residual,dropout=args.dropout)
     #start training
-    bestmodel=test(model,trainloader=train_loader,valloader=val_loader,testloader=test_loader,epochs=args.n_epoch,args=args)
+    bestmodel=train(model,trainloader=train_loader,valloader=val_loader,testloader=test_loader,epochs=args.n_epoch,args=args)
